010d4a11053516f346a1831e6bfd6c15529a87a7
pranjalsingla <88027633+pranjalsingla@users.noreply.github.com>
2022-09-25 09:50:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -11,6 +15,11 @@ function setup(){
 
 function draw(){
     background('#f2f0c7');
+    document.getElementById("font_size").innerHTML = "Font size of the text is "+ difference+" px";
+    textSize(difference);
+    fill('#244A4A');
+    text('Pranjal',29,270);
+
 }
 
 function modelLoaded(){
@@ -20,5 +29,10 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX-rightWristX);
+        console.log("left wrist x = "+leftWristX+", right wrist x = "+rightWristX+", difference = "+ difference);
+        
     }
 }
